87ff001868482cedd0f4ff1da0a524a39fca9cfc
quang709 <quangquangquang321@gmail.com>
2021-01-06 09:54:08 +0700

    tim min max

---
+++ main.py
@@ -0,0 +1,16 @@
+a = [1,3,5,-9,-15,0, 7,100]
+gia_tri_min = a[0]
+for item in a:
+    if gia_tri_min > item:
+        gia_tri_min = item
+print('Gia tri nho nhat la: ', gia_tri_min)
+
+
+
+
+b = [1,3,5,-9,-15,0, 7,100]
+gia_tri_max = b[0]
+for item in b:
+    if gia_tri_max < item:
+        gia_tri_max = item
+print('Gia tri lon nhat la: ', gia_tri_max)
